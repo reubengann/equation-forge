ae0e14a2451f7d881db0c6d67f019a42990f76e5
Reuben Gann <reubengann@gmail.com>
2026-06-10 10:48:19 -0400

    cleanup

--- output.ps1
+++
@@ -1,7 +0,0 @@
-Get-ChildItem -Recurse -File -Include *.ts,*.tsx,*.md |
-  Where-Object { $_.FullName -notmatch "node_modules|dist|build" } |
-  Sort-Object FullName |
-  ForEach-Object {
-    "`n`n===== FILE: $($_.FullName) =====`n"
-    Get-Content $_
-  } | Set-Content project-bundle.txt
